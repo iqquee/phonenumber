ad1760086e25be63f37fd8fd3dd267e9d7f860df
hisyntax <hisyntax0@gmail.com>
2022-12-16 10:57:18 +0100

    symbols added to the array of alphaNum

--- phone-number.go
+++ phone-number.go
@@ -9,7 +9,10 @@ func Parse(number string) (string, error) {
 	constAlpha := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
 		"k", "l", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
 		"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O",
-		"P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
+		"P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+		"!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "{", "}", ":", ";",
+		"'", "|", "<", ">", "?", "/", ".", ",",
+	}
 
 	var newNum string
 	var avail bool
